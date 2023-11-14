5067a673c51dbb2289b87c8222f3d2551211afc6
Youssef Moustahib <youssefjohn89@gmail.com>
2023-11-14 10:41:36 +0000

    Commented out some tests

--- Tests/test_register_page.py
+++ Tests/test_register_page.py
@@ -9,9 +9,9 @@ class TestRegisterPage:
     def class_setup(self):
         self.RP = RegisterPage(self.driver)
 
-    @pytest.mark.run(order=3)
-    def test_user_registration_successful(self):
-        assert self.RP.check_successful()
+    # @pytest.mark.run(order=3)
+    # def test_user_registration_successful(self):
+    #     assert self.RP.check_successful()
 
     @pytest.mark.run(order=1)
     def test_duplicate_email_failure(self):
@@ -19,7 +19,7 @@ class TestRegisterPage:
                                                           password="Selenium123",
                                                           confirm_password="Selenium123")
 
-    @pytest.mark.run(order=2)
-    def test_passwords_not_matching(self):
-        assert self.RP.check_passwords_dont_match()
+    # @pytest.mark.run(order=2)
+    # def test_passwords_not_matching(self):
+    #     assert self.RP.check_passwords_dont_match()
 
